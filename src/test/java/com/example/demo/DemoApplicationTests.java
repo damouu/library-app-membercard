34d8fc64409a32f19be233f09994720e6eb54c2c
damouu <mouadsehbaoui@gmail.com>
2025-11-18 23:35:51 +0100

    test
    Tested-by: damou_ <mouadsehbaoui@gmail.com>
    
    Reviewed-by: damou_ <mouadsehbaoui@gmail.com>

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,9 +2,15 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class DemoApplicationTests {
+
+
+    @MockBean
+    private JwtDecoder jwtDecoder;
 
     @Test
     void contextLoads() {
