020a94ae48ec56222385c638601f30c7d7dc1ad6
damouu <mouadsehbaoui@gmail.com>
2025-12-07 16:33:25 +0100

    Refactor: DemoApplicationTests.java (#LAM-1)

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,16 +2,10 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class DemoApplicationTests {
-
-
-    @MockBean
-    private JwtDecoder jwtDecoder;
-
+    
     @Test
     void contextLoads() {
     }
