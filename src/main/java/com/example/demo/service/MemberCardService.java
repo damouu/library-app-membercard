0a32bde8f7f3a8744fb52d6e9566d51ec366bf69
damouu <mouadsehbaoui@gmail.com>
2026-01-19 17:09:52 +0100

    Test: MemberCardService (#LAM-2)

--- src/main/java/com/example/demo/service/MemberCardService.java
+++ src/main/java/com/example/demo/service/MemberCardService.java
@@ -2,9 +2,8 @@ package com.example.demo.service;
 
 import com.example.demo.model.MemberCard;
 import com.example.demo.repository.MemberCardRepository;
-import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -14,26 +13,23 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
-@Data
+@RequiredArgsConstructor
 public class MemberCardService {
 
     private final MemberCardRepository memberCardRepository;
-
 
     public void deleteMemberCard(Map<String, String> data) throws ResponseStatusException {
         UUID uuid = UUID.fromString(data.get("memberCardUUID"));
         Optional<MemberCard> memberCard = Optional.ofNullable(memberCardRepository.findMemberCardByUuid(uuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "member card does not exist")));
         memberCard.get().setDeleted_at(LocalDateTime.now());
         memberCardRepository.save(memberCard.get());
-        ResponseEntity.status(204).build();
     }
 
-    public ResponseEntity<MemberCard> postMemberCard(Map<String, String> data) throws ResponseStatusException {
+    public void postMemberCard(Map<String, String> data) throws ResponseStatusException {
         UUID uuid = UUID.fromString(data.get("memberCardUUID"));
         MemberCard memberCard = new MemberCard(uuid);
         memberCard.setCreatedAT(LocalDateTime.now());
         memberCard.setValidUntil(LocalDateTime.now().plusYears(2));
         memberCardRepository.save(memberCard);
-        return ResponseEntity.status(204).build();
     }
 }
